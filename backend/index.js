45d7536cca64646379459c11081d360e9061267a
Weber Souza <weberv.souza@gmail.com>
2024-08-02 06:26:49 +0800

    Add files via upload

--- backend/index.js
+++ backend/index.js
@@ -80,7 +80,7 @@ const Product = mongoose.model("Product",{
 app.post('/addproduct',async (req,res)=>{
     let products = await Product.find({});           // 
     let id;
-    if(product.length>0){
+    if(products.length>0){
         let last_product_array = products.slice(-1);
         let last_product = last_product_array[0];
         id=last_product.id+1;
@@ -88,7 +88,7 @@ app.post('/addproduct',async (req,res)=>{
         id=1;
     }
     const product = new Product({
-        id:re,
+        id:id,
         name:req.body.name,
         image:req.body.image,
         category:req.body.category,
@@ -191,9 +191,9 @@ app.post('/login', async(req,res)=>{
 })
 
 // Creating endpoint for newcollection data 
-app.get('/newcollection', async(req,res)=>{
-    let product = await Product.find({});
-    let newcollection = products.slice(1).slide(-8);
+app.get('/newcollections', async(req,res)=>{
+    let products = await Product.find({});
+    let newcollection = products.slice(1).slice(-8);
     console.log("NewCollection Fetched");
     res.send(newcollection);
 })
@@ -206,24 +206,24 @@ app.get('/popularinwomen',async(req,res)=>{
     res.send(popular_in_women);
 })
 // Creating middelware to fetch user
-    const fetchUser = async (req,res,next)=>{
-        const token = req.header('auth-token');
-        if (!token) {
-            res.status(401).send({errors:"Please authenticate using valid token"})
-        }else{
-            try {
-                const data = jwt.verify(token,'secret_ecom');
-                req.user = data.user;
-                next();
-            } catch (error) {
-                res.status(401).send({errors:"Please authenticate using a valid token"})
-            }
+const fetchUser = async (req,res,next)=>{
+    const token = req.header('auth-token');
+    if (!token) {
+        res.status(401).send({errors:"Please authenticate using valid token"})
+    }else{
+        try {
+            const data = jwt.verify(token,'secret_ecom');
+            req.user = data.user;
+            next();
+        } catch (error) {
+            res.status(401).send({errors:"Please authenticate using a valid token"})
         }
     }
+}
 // Creating endpoint for adding products in cartdata
 app.post('/addtocart',fetchUser,async (req,res)=>{
     console.log("added",req.body.itemId);
-    let userData = await Users.findOne({_id:rew.user.id});
+    let userData = await Users.findOne({_id:req.user.id});
     userData.cartData[req.body.itemId] += 1;
     await Users.findOneAndUpdate({_id:req.user.id},{cartData:userData.cartData});
     res.send("Added")
@@ -232,7 +232,7 @@ app.post('/addtocart',fetchUser,async (req,res)=>{
 // Creating endpoint to remove product from cartdata
 app.post('/removefromcart',fetchUser,async(req,res)=>{
     console.log("removed",req.body.itemId);
-    let userData = await Users.findOne({_id:rew.user.id});
+    let userData = await Users.findOne({_id:req.user.id});
     if(userData.cartData[req.body.itemId]>0)
     userData.cartData[req.body.itemId] -= 1;
     await Users.findByIdAndUpdate({_id:req.user.id},{cartData:userData.cartData});
@@ -242,7 +242,7 @@ app.post('/removefromcart',fetchUser,async(req,res)=>{
 // Creating endpoint to get cartdata
 app.post('/getcart',fetchUser,async(req,res)=>{
     console.log("GetCart");
-    let userData = await Users.findOne({_id:rew.user.id});
+    let userData = await Users.findOne({_id:req.user.id});
     res.json(userData.cartData);
 })
 
